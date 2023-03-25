1a9d1912dacdce3555f329822b3e4acb0326c122
hucancode <hucancode@gmail.com>
2023-03-25 13:52:32 +0900

    better messages

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ fn main() {
     let hand_b = args.get(3).unwrap_or(empty);
     println!(
         "Running numbers... \n\n\
-        Community cards: {:>12}\n\
-        Your hand:       {:>12}\n\
-        Their hand:      {:>12}\n",
+        🎴 Community cards: {:>12}\n\
+        🎴 Your hand:       {:>12}\n\
+        🎴 Their hand:      {:>12}\n",
         HandConverter::pretify(community),
         HandConverter::pretify(hand_a),
         HandConverter::pretify(hand_b),
@@ -25,11 +25,11 @@ fn main() {
     let (win, lose, tie) = game.solve();
     let win_rate = win as f32 / (win + lose + tie) as f32 * 100.0;
     println!(
-        "_____________________________\n\
-        Win:               {:>10}\n\
-        Lose:              {:>10}\n\
-        Tie:               {:>10}\n\
-        You win:           {:>9}%",
+        "________________________________\n\
+        👑 Win:                {:>10}\n\
+        💸 Lose:               {:>10}\n\
+        🤝 Tie:                {:>10}\n\
+        🧮 You win:            {:>9}%",
         win, lose, tie, win_rate
     );
 }

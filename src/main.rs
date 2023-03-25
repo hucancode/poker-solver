e0aa4a65abdfef4e5d75a1b4cb85138d5802f91c
hucancode <hucancode@gmail.com>
2023-03-25 14:16:35 +0900

    better messages

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod poker;
 use poker::Game;
 use poker::HandConverter;
 use std::env;
+use std::io::stdout;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,15 +11,20 @@ fn main() {
     let community = args.get(1).unwrap_or(empty);
     let hand_a = args.get(2).unwrap_or(empty);
     let hand_b = args.get(3).unwrap_or(empty);
-    println!(
-        "Running numbers... \n\n\
+    print!(
+        "\n\
         🎴 Community cards: {:>12}\n\
         🎴 Your hand:       {:>12}\n\
-        🎴 Their hand:      {:>12}\n",
+        🎴 Their hand:      {:>12}\n\
+        \n\
+        Running numbers...",
         HandConverter::pretify(community),
         HandConverter::pretify(hand_a),
         HandConverter::pretify(hand_b),
     );
+    if stdout().flush().is_err() {
+        return;
+    }
     let game = Game::new()
         .with_hand_a(hand_a)
         .with_hand_b(hand_b)
@@ -25,11 +32,11 @@ fn main() {
     let (win, lose, tie) = game.solve();
     let win_rate = win as f32 / (win + lose + tie) as f32 * 100.0;
     println!(
-        "________________________________\n\
-        👑 Win:                {:>10}\n\
-        💸 Lose:               {:>10}\n\
-        🤝 Tie:                {:>10}\n\
-        🧮 You win:            {:>9}%",
+        "\r\
+        👑 Win:               {:>10}\n\
+        💸 Lose:              {:>10}\n\
+        🤝 Tie:               {:>10}\n\
+        🧮 You win:           {:>9}%",
         win, lose, tie, win_rate
     );
 }

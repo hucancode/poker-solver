cdba85014c89e3b970a9d1d64ef6ff48493cb4af
hucancode <hucancode@gmail.com>
2023-04-12 10:25:51 +0900

    add wasm target

--- src/poker/mod.rs
+++ src/poker/mod.rs
@@ -1,3 +1,15 @@
 pub mod evaluator;
 pub mod game;
 pub mod hand;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, hello-wasm!");
+}

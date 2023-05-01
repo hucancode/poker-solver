b4c596cb34cd30a861737592f46b2d504ac52062
hucancode <hucancode@gmail.com>
2023-05-01 15:46:19 +0900

    more tests

--- src/utils.rs
+++ src/utils.rs
@@ -17,8 +17,19 @@ pub fn prettify(hand: &str) -> String {
 mod tests {
     #[test]
     fn prettify() {
+        let output = super::prettify("3s2c4h6sAd");
+        let expected = "3♠2♣4♥6♠A♦";
+        assert_eq!(output, expected);
         let output = super::prettify("QsQcQhKsKd");
         let expected = "Q♠Q♣Q♥K♠K♦";
+        assert_eq!(output, expected);
+        let output = super::prettify("");
+        let expected = "";
+        assert_eq!(output, expected);
+        let output = super::prettify("2s2c2d2h");
+        let expected = "2♠2♣2♦2♥";
+        let output = super::prettify("2s3s4s5s6s7s8s9sTsJsQsKsAs");
+        let expected = "2♠3♠4♠5♠6♠7♠8♠9♠10♠J♠Q♠K♠A♠";
         assert_eq!(output, expected);
     }
 }

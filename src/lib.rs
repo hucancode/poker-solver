16164816e2c8407f18a3333fa34c5975ffedf4ba
hucancode <hucancode@gmail.com>
2023-06-02 23:29:07 +0900

    code format

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,17 @@ pub struct GameResult {
     pub lose: usize,
     pub tie: usize,
 }
-static GAME_INSTANCE: OnceLock<Mutex<Game>> = OnceLock::new();
 
 #[wasm_bindgen]
 pub fn solve(hand_a: String, hand_b: String, community: String) -> GameResult {
+    static GAME_INSTANCE: OnceLock<Mutex<Game>> = OnceLock::new();
     let mutex = GAME_INSTANCE.get_or_init(|| Mutex::new(Game::new()));
     if let Ok(mut game) = mutex.lock() {
-        match game.solve_by(hand_a.as_str(), hand_b.as_str(), community.as_str()) {
-            Ok((win, lose, tie)) => GameResult { win, lose, tie },
-            _ => GameResult::default(),
+        if let Ok((win, lose, tie)) =
+            game.solve_by(hand_a.as_str(), hand_b.as_str(), community.as_str())
+        {
+            return GameResult { win, lose, tie };
         }
-    } else {
-        GameResult::default()
     }
+    GameResult::default()
 }
--- src/main.rs
+++ src/main.rs
@@ -14,44 +14,43 @@ fn main() {
     let community = args.get(1).unwrap_or(empty);
     let hand_a = args.get(2).unwrap_or(empty);
     let hand_b = args.get(3).unwrap_or(empty);
-    print!(
-        "\n\
-        🎴 Community cards: {:>12}\n\
-        🎴 Your hand:       {:>12}\n\
-        🎴 Their hand:      {:>12}\n\
-        \n\
-        Running numbers...",
-        prettify(community),
-        prettify(hand_a),
-        prettify(hand_b),
-    );
-    if stdout().flush().is_err() {
-        return;
-    }
-    match solve(hand_a, hand_b, community) {
-        Ok((win, lose, tie)) => {
-            let win_rate = win as f32 / (win + lose + tie) as f32 * 100.0;
-            println!(
-                "\r\
-                👑 Win:               {win:>10}\n\
-                💸 Lose:              {lose:>10}\n\
-                🤝 Tie:               {tie:>10}\n\
-                🧮 You win:           {win_rate:>9}%"
-            );
-        }
-        Err(e) => {
-            println!("\r{:^32}\n", e);
-        }
-    }
+    solve(hand_a, hand_b, community);
 }
 
-static GAME_INSTANCE: OnceLock<Mutex<Game>> = OnceLock::new();
-
-pub fn solve(hand_a: &str, hand_b: &str, community: &str) -> Result<(usize, usize, usize), String> {
+fn solve(hand_a: &str, hand_b: &str, community: &str) {
+    static GAME_INSTANCE: OnceLock<Mutex<Game>> = OnceLock::new();
     let mutex = GAME_INSTANCE.get_or_init(|| Mutex::new(Game::new()));
     if let Ok(mut game) = mutex.lock() {
-        game.solve_by(hand_a, hand_b, community)
+        print!(
+            "\n\
+            🎴 Community cards: {:>12}\n\
+            🎴 Your hand:       {:>12}\n\
+            🎴 Their hand:      {:>12}\n\
+            \n\
+            Running numbers...",
+            prettify(community),
+            prettify(hand_a),
+            prettify(hand_b),
+        );
+        if stdout().flush().is_err() {
+            return;
+        }
+        match game.solve_by(hand_a, hand_b, community) {
+            Ok((win, lose, tie)) => {
+                let win_rate = win as f32 / (win + lose + tie) as f32 * 100.0;
+                println!(
+                    "\r\
+                    👑 Win:               {win:>10}\n\
+                    💸 Lose:              {lose:>10}\n\
+                    🤝 Tie:               {tie:>10}\n\
+                    🧮 You win:           {win_rate:>9}%"
+                );
+            }
+            Err(e) => {
+                println!("\r{:^32}\n", e);
+            }
+        };
     } else {
-        Err("could not get game solver instance".to_string())
+        println!("could not get game solver instance");
     }
 }
